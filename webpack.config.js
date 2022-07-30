8972bfe2ac367026d5e187d0a9feb0c195b32f2c
crbac3217 <crbac3217@gmail.com>
2022-07-30 14:36:06 -0400

    initial comm

--- webpack.config.js
+++ webpack.config.js
@@ -6,7 +6,7 @@ const MiniCSSExtractPlugin = require('mini-css-extract-plugin')
 module.exports = {
     entry: path.resolve(__dirname, './src/index.js'),
     output:{
-        path: path.resolve(__dirname, '../dist'),
+        path: path.resolve(__dirname, './dist'),
         filename: 'bundle.[contenthash].js',
     },
     devServer: {
