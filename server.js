b05d73f3b0972df4409038ca3179e380005241bf
crbac3217 <crbac3217@gmail.com>
2022-07-30 15:50:16 -0400

    initial comm

--- server.js
+++ server.js
@@ -9,3 +9,7 @@ app.set('port', process.env.PORT || 3000);
 var server = app.listen(app.get('port'), function() {
   console.log('listening on port ', server.address().port);
 });
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '/dist/index.html'));
+  });
